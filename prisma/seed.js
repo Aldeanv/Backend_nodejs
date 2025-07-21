2a3febf079baefe962d6e47bb762642c4e335724
Aldeanv <alvitodennova@gmail.com>
2025-07-21 18:38:52 +0700

    chore: setup prisma seeder add admin seed

---
+++ prisma/seed.js
@@ -0,0 +1,29 @@
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcryptjs');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const hashedPassword = await bcrypt.hash('admin456', 10);
+  await prisma.user.create({
+    data: {
+      name: 'Admin4',
+      email: 'admin4@example.com',
+      password: hashedPassword,
+      role: 'admin',
+      NIK: '1234567390123456',
+      phone: '0812323567890'
+    },
+  });
+
+  console.log('Admin user seeded.');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
